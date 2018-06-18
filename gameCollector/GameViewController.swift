57c29106189652e5120ba25cc724641adee30cfb
jonmoon74 <jon.moon@outlook.com>
2018-06-18 21:06:42 +0100

    Set up of viewControllers

---
+++ gameCollector/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  gameCollector
+//
+//  Created by Jon Moon on 18/06/2018.
+//  Copyright © 2018 Jon Moon. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
