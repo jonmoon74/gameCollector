b9a7fe2877a5bcf326f3f649eb8f8ec147404a7a
jonmoon74 <jon.moon@outlook.com>
2018-06-19 21:01:57 +0100

    photopicker added

--- gameCollector/GameViewController.swift
+++ gameCollector/GameViewController.swift
@@ -8,21 +8,30 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var gameImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
     }
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
+    
     @IBAction func addTapped(_ sender: Any) {
     }
+    
 }
