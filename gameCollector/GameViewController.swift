b0227500d6805575b231830952bc130bdbd157d1
jonmoon74 <jon.moon@outlook.com>
2018-06-21 21:11:57 +0100

    CoreData added

--- gameCollector/GameViewController.swift
+++ gameCollector/GameViewController.swift
@@ -28,10 +28,24 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        gameImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
     
     @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let game = Game(context: context)
+        game.title = titleTextField.text
+        game.image = UIImagePNGRepresentation(gameImageView.image!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
     
 }
