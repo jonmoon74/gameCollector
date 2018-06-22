a88feb545038298f3aa16e663915233d7be42375
jonmoon74 <jon.moon@outlook.com>
2018-06-22 21:11:26 +0100

    Started the table view but not complete yet

--- gameCollector/ViewController.swift
+++ gameCollector/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var game : [Game] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
     }
 
 
